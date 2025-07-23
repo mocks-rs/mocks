a7300e4dcffb280a169b5f56cc08fe1959587a6e
Kazuno Fukuda <4kz12zz@gmail.com>
2025-07-23 22:13:58 +0900

    feat: Delete npm-dist

--- npm-dist/mocks-linux-x64/index.js
+++
@@ -1,6 +0,0 @@
-const path = require('path');
-
-module.exports = {
-  platform: 'linux-x64',
-  binaryPath: path.join(__dirname, 'mocks')
-};
--- npm-dist/mocks-win32-x64/index.js
+++
@@ -1,6 +0,0 @@
-const path = require('path');
-
-module.exports = {
-  platform: 'win32-x64',
-  binaryPath: path.join(__dirname, 'mocks.exe')
-};
